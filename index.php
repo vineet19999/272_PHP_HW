3b7fcf4bdc986174d160a9f67f40c6b351758515
vineet19999 <145827396+vineet19999@users.noreply.github.com>
2024-05-10 15:58:00 -0700

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Shreek's Cloud8Kit</title>
-    <link rel="stylesheet" type="text/css" href="styles.css">
+    <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
     <header>
